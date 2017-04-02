23b4278d815529df6146245f3f8e16a869507c21
johnytiago <joaosao10@gmail.com>
2017-04-02 16:35:31 +0100

    Adds envelope to message and serialization methods

--- src/java/message/Message.java
+++ src/java/message/Message.java
@@ -1,31 +1,67 @@
 package message;
 
 import java.security.Key;
+import java.io.Serializable;
+
+import org.apache.commons.lang3.SerializationUtils;
 
 public class Message {
 
-  public byte[] domain;
-  public byte[] username;
-  public byte[] password;
-  public byte[] usernameHash;
-  public byte[] domainHash;
-  public int counter;
-  public byte[] publicKey;
+  public Envelope envelope;
+  public byte[] hmac;
 
-  public void message( Key publicKey,
-                       byte[] domain,
-                       byte[] username,
-                       byte[] password,
-                       byte[] usernameHash,
-                       byte[] domainHash,
-                       int counter) { 
+  public Message(byte[] envelope){
+    this.envelope = (Envelope) SerializationUtils.deserialize(envelope);
+  }
 
-    this.publicKey = publicKey.getEncoded();
-    this.domain = domain;
-    this.username = username;
-    this.password = password;
-    this.usernameHash = usernameHash;
-    this.domainHash = domainHash;
-    this.counter = counter;
+  public Message(
+      Key publicKey,
+      byte[] domain,
+      byte[] username,
+      byte[] password,
+      byte[] usernameHash,
+      byte[] domainHash,
+      int counter) { 
+
+    this.envelope = new Envelope(publicKey, domain, username, password, usernameHash, domainHash, counter);
+  }
+
+  public byte[] serialize(){
+    return SerializationUtils.serialize(this.envelope);
+  }
+
+  public void setHMAC( byte[] hmac ) {
+    this.hmac = hmac;
+  }
+
+  public class Envelope implements Serializable{
+
+    public byte[] domain;
+    public byte[] username;
+    public byte[] password;
+    public byte[] usernameHash;
+    public byte[] domainHash;
+    public int counter;
+    public byte[] publicKey;
+    
+    private static final long serialVersionUID = 1L;
+    
+    public Envelope(
+        Key publicKey,
+        byte[] domain,
+        byte[] username,
+        byte[] password,
+        byte[] usernameHash,
+        byte[] domainHash,
+        int counter) { 
+
+      this.publicKey = publicKey.getEncoded();
+      this.domain = domain;
+      this.username = username;
+      this.password = password;
+      this.usernameHash = usernameHash;
+      this.domainHash = domainHash;
+      this.counter = counter;
+        }
   }
 }
