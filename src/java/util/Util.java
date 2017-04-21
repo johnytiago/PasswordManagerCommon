ee0e9bad890bf0dedbf004a6ec90dd651ced2110
johnytiago <joaosao10@gmail.com>
2017-04-21 11:45:40 +0100

    null check before concatenation at util

--- src/java/util/Util.java
+++ src/java/util/Util.java
@@ -8,10 +8,14 @@ public class Util {
   public byte[] msgToByteArray(Message msg) {
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
     try {
-      outputStream.write( msg.publicKey );
-      outputStream.write( msg.usernameHash );
-      outputStream.write( msg.password );
-      outputStream.write( msg.tripletHash );
+      if ( msg.publicKey != null)
+        outputStream.write( msg.publicKey );
+      if ( msg.usernameHash != null)
+        outputStream.write( msg.usernameHash );
+      if ( msg.password != null)
+        outputStream.write( msg.password );
+      if ( msg.tripletHash != null)
+        outputStream.write( msg.tripletHash );
       outputStream.write( msg.counter );
       return outputStream.toByteArray();
     } catch( Exception e) {
