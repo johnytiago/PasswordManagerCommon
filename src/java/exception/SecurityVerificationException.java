34a6fd9dfb95af7872d38bbb99c155149528a2be
johnytiago <joaosao10@gmail.com>
2017-04-29 18:56:23 +0100

    Removes message parameter

--- src/java/exception/SecurityVerificationException.java
+++ src/java/exception/SecurityVerificationException.java
@@ -2,14 +2,14 @@ package exception;
 
 public class SecurityVerificationException extends PasswordManagerException{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4190326892510022832L;
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -4190326892510022832L;
 
-	public SecurityVerificationException(String message) {
-		super("Security verifications failed.");
-		// TODO Auto-generated constructor stub
-	}
+  public SecurityVerificationException() {
+    super("Security verifications failed.");
+    // TODO Auto-generated constructor stub
+  }
 
 }
